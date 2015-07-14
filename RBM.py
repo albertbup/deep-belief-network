c1bbd364d583149aa5acf12214ba297d3365b386
Albert Busqué <albert.busque@infantium.com>
2015-07-14 14:00:45 +0200

    implemented the computing of visible and hidden units.

--- RBM.py
+++ RBM.py
@@ -3,17 +3,21 @@ import numpy as np
 
 class RBM():
     def __init__(self, data, num_hidden_units):
-        self.num_visible_units = data.shape[0] + 1
-        self.num_hidden_units = num_hidden_units + 1
-        self.W = np.zeros([self.num_hidden_units, self.num_visible_units])
+        self.num_visible_units = data.shape[0]
+        self.num_hidden_units = num_hidden_units
+        self.W = np.random.randn(self.num_hidden_units, self.num_visible_units)
         self.H = np.zeros(self.num_hidden_units)
         self.V = np.zeros(self.num_visible_units)
+        self.c = np.random.randn(self.num_hidden_units)
+        self.b = np.random.randn(self.num_visible_units)
 
-    def sigmoid(self, vector):
-        func = np.vectorize(self.__sigmoid)
+    @classmethod
+    def sigmoid(cls, vector):
+        func = np.vectorize(cls.__sigmoid)
         return func(vector)
 
-    def __sigmoid(self, x):
+    @classmethod
+    def __sigmoid(cls, x):
         return 1 / (1.0 + np.exp(-x))
 
     def fit(self):
@@ -25,8 +29,10 @@ class RBM():
     def __contrastive_divergence(self, k=1):
         return
 
-    def __compute_hidden_units(self):
-        return
+    def __compute_hidden_units(self, matrix_weights, vector_visible_units, vector_bias_hidden):
+        W, v, c = matrix_weights, vector_visible_units, vector_bias_hidden
+        return self.__sigmoid(np.dot(W, v) + c)
 
-    def __compute_visible_units(self):
-        return
+    def __compute_visible_units(self, matrix_weights, vector_hidden_units, vector_bias_visible):
+        W, h, b = matrix_weights, vector_hidden_units, vector_bias_visible
+        return self.__sigmoid(np.dot(h, W) + b)
