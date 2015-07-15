bb8fcada21da2a04e6e11e4025cbf001683c46ee
Albert <albert.busque@infantium.com>
2015-07-15 15:24:57 +0200

    minor refactoring

--- RBM.py
+++ RBM.py
@@ -30,14 +30,14 @@ class RBM():
     def __stochastic_gradient_descent(self, data, learning_rate, iterations):
         for it in range(1, iterations + 1):
             np.random.shuffle(data)
+            #W0 = np.copy(self.W)
             for sample in data:
                 delta_W, delta_b, delta_c = self.__contrastive_divergence(sample)
-                # W0 = np.copy(self.W)
-                self.W = self.W + learning_rate * delta_W
-                self.b = self.b + learning_rate * delta_b
-                self.c = self.c + learning_rate * delta_c
-                # diff = np.mean(np.abs(W0 - self.W))
-                # print ">> Mean diff %f finished" % diff
+                self.W += learning_rate * delta_W
+                self.b += learning_rate * delta_b
+                self.c += learning_rate * delta_c
+            #diff = np.mean(np.abs(W0 - self.W))
+            #print ">> Mean diff %f finished" % diff
             print ">> Epoch %d finished" % it
 
     def __contrastive_divergence(self, vector_visible_units, k=1):
@@ -49,13 +49,13 @@ class RBM():
 
         # Sampling
         for t in range(k):
-            h_t = self.__compute_hidden_units(self.W, v_t, self.c)
-            v_t = self.__compute_visible_units(self.W, h_t, self.b)
+            h_t = self.__compute_hidden_units(v_t)
+            v_t = self.__compute_visible_units(h_t)
 
         # Computing deltas
         v_k = v_t
-        h_0 = self.__compute_hidden_units(self.W, v_0, self.c)
-        h_k = self.__compute_hidden_units(self.W, v_k, self.c)
+        h_0 = self.__compute_hidden_units(v_0)
+        h_k = self.__compute_hidden_units(v_k)
         for i in range(self.num_hidden_units):
             for j in range(self.num_visible_units):
                 delta_W[i][j] += h_0[i] * v_0[j] - h_k[i] * v_k[j]
@@ -65,10 +65,10 @@ class RBM():
 
         return delta_W, delta_b, delta_c
 
-    def __compute_hidden_units(self, matrix_weights, vector_visible_units, vector_bias_hidden):
-        W, v, c = matrix_weights, vector_visible_units, vector_bias_hidden
-        return self.__sigmoid(np.dot(W, v) + c)
+    def __compute_hidden_units(self, vector_visible_units):
+        v = vector_visible_units
+        return self.__sigmoid(np.dot(self.W, v) + self.c)
 
-    def __compute_visible_units(self, matrix_weights, vector_hidden_units, vector_bias_visible):
-        W, h, b = matrix_weights, vector_hidden_units, vector_bias_visible
-        return self.__sigmoid(np.dot(h, W) + b)
+    def __compute_visible_units(self, vector_hidden_units):
+        h = vector_hidden_units
+        return self.__sigmoid(np.dot(h, self.W) + self.b)
