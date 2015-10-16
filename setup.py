98e4bd0b87f1447e618a90cf07f96ce7aac75a17
Albert Busqué <albert.busque@infantium.com>
2015-10-16 10:21:49 +0200

    requierements in setup.py

--- setup.py
+++ setup.py
@@ -6,4 +6,5 @@ setup(name='deep-belief-network',
       version='0.1',
       description='Python implementation of Deep Belief Networks',
       packages=['dbn'],
+      install_requires=['scikit-learn>=0.16.1', 'numpy>=1.9.2']
       )
