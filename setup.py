e83e798095d222b5e65bd341ef496ab6a3c56b92
Albert <albert.busque@infantium.com>
2016-11-09 17:07:11 +0100

    update version and add tensorflow as requierement

--- setup.py
+++ setup.py
@@ -3,8 +3,11 @@
 from distutils.core import setup
 
 setup(name='deep-belief-network',
-      version='0.1.15',
+      version='0.2.0',
       description='Python implementation of Deep Belief Networks',
       packages=['dbn'],
-      install_requires=['numpy>=1.9.2', 'scipy>=0.16.1', 'scikit-learn>=0.16.1']
+      install_requires=['numpy>=1.9.2',
+                        'scipy>=0.16.1',
+                        'scikit-learn>=0.16.1',
+                        'tensorflow>=0.9.0']
       )
