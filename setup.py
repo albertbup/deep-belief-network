f130af5dad13a7fb3cad6a62d70daf6fc513dedf
Albert <albert.busque@infantium.com>
2016-11-11 00:25:51 +0100

    version upgrade

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='deep-belief-network',
-      version='0.2.0',
+      version='0.2.1',
       description='Python implementation of Deep Belief Networks',
       packages=['dbn'],
       install_requires=['numpy>=1.9.2',
