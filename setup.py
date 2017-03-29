2ef37d40118adfe4379d934b0a39e6ac52449969
albert <albert@iiia.csic.es>
2017-03-29 10:48:29 +0200

    remove hard-coded requirements

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@
 
 from distutils.core import setup
 
+with open('requirements.txt') as fp:
+    requirements = fp.read().splitlines()
+
 setup(name='deep-belief-network',
       version='0.7.0',
       description='Python implementation of Deep Belief Networks',
       packages=['dbn'],
-      install_requires=['numpy==1.12.0',
-                        'scipy==0.18.1',
-                        'scikit-learn==0.18.1',
-                        'tensorflow==1.0.0'
-                        ]
+      install_requires=requirements,
       )
