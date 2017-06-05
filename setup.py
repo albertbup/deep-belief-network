2f358251f017d6d79e6d65d224066632ee2e61bf
albert <albert.busque@gmail.com>
2017-06-05 16:29:58 +0200

    version upgrade

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as fp:
     requirements = fp.read().splitlines()
 
 setup(name='deep-belief-network',
-      version='1.0.0',
+      version='1.0.1',
       description='Python implementation of Deep Belief Networks',
       packages=['dbn', 'dbn.tensorflow'],
       install_requires=requirements,
